fd129b161909f6188019128d12a37711d99eddf3
Chenng <ringcrl@foxmail.com>
2018-04-03 22:44:33 +0800

    [+]补上 enter 的 class

--- main.js
+++ main.js
@@ -1,46 +1,43 @@
-let n
-初始化()
-setInterval(() =>{
-  makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-        makeEnter($(e.currentTarger))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1  
-},2000)
+let n = 1;
+init();
+setInterval(() => {
+  makeLeave(getImage(n)).one('transitionend', (e) => { makeEnter($(e.currentTarger)); });
+  makeCurrent(getImage(n + 1));
+  makeEnter(getImage(n - 1)); // 走完之后，在给前一个补一个 enter
+  n += 1;
+}, 2000);
 
 
-
-
-
-function getImage(n){
-    return $(`.images > img:nth-child(${x(n)})`)
+function getImage(n) {
+  return $(`.images > img:nth-child(${get_current(n)})`);
 }
 
-function x(n){
-    if (n>3){
-        n = n%3
-        if (n === 0){
-            n = 3
-        }
+function get_current(n) {
+  if (n > 3) {
+    n = n % 3;
+    if (n === 0) {
+      n = 3;
     }
-    return n
+  }
+  return n;
 }
 
-function 初始化(){
-    n = 1
-    $(`.images > img:nth-child(${x(n)})`).addClass('current')
-      .siblings().addClass('enter')
+function init() {
+  $(`.images > img:nth-child(${get_current(n)})`)
+    .addClass('current')
+    .siblings()
+    .addClass('enter');
+    
 }
 
-function makeCurrent($node){
-    return $node.removeClass('enter leave').addClass('current')
+function makeCurrent($node) {
+  return $node.removeClass('enter leave').addClass('current');
 }
 
-function makeLeave($node){
-    return $node.removeClass('enter current').addClass('leave')
+function makeLeave($node) {
+  return $node.removeClass('enter current').addClass('leave');
 }
 
-function makeEnter($node){
-    return $node.removeClass('leave current').addClass('enter')
+function makeEnter($node) {
+  return $node.removeClass('leave current').addClass('enter');
 }
